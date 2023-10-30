cec2c6534913ae7b47532ba0236483acb0e89f24
chipy <iamchipy@gmail.com>
2023-10-30 11:08:32 -0400

    Local States functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider} from 'react-router-dom'
+import store from './store'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
@@ -24,7 +26,10 @@ const router = createBrowserRouter(
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={(store)}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 )
+  
---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    // check for user info, ifwe have it parse it or else it's null
+    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')): null
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setCredentials: (state, action) => {
+            state.userInfo = action.payload
+            localStorage.setItem('userInfo', JSON.stringify(action.payload))
+        },
+        logout: (state, action) => {
+            state.userInfo = null
+            localStorage.removeItem('userInfo')
+        }
+    }
+})
+
+export const { setCredentials, logout } = authSlice.actions
+export default authSlice.reducer
