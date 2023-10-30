396227898f8ed97af87b088ea83c09b49378172c
chipy <iamchipy@gmail.com>
2023-10-30 10:43:29 -0400

    PageNav working with forms Pre-States

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,12 +9,16 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
+import LoginScreen from './screens/LoginScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx'
 
 {/* ALL ROUTES LIVES HERE */}
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path = '/' element= {<App />}>
       <Route index={true} path = '/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
     </Route>
   ))
 
