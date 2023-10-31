fd64afb0ab2c7aecd698e58f015222c26c5c19d6
chipy <iamchipy@gmail.com>
2023-10-31 14:06:38 -0400

    Discord URI for auth corrected

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -19,7 +19,8 @@ const LoginScreen = () => {
 
     const { userInfo } = useSelector((state) => state.auth)
 
-    const discordOAuthURI = "https://discord.com/api/oauth2/authorize?client_id=1168939215367721021&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth&response_type=token&scope=identify%20guilds"
+    // const discordOAuthURI ="https://discord.com/api/oauth2/authorize?client_id=1168939215367721021&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth&response_type=token&scope=identify%20guilds"
+    const discordOAuthURI ="https://discord.com/api/oauth2/authorize?client_id=1168939215367721021&redirect_uri=https%3A%2F%2Fbreeding.chipy.dev%2Foauth&response_type=token&scope=identify%20guilds"
 
     useEffect(() => {
         if (userInfo) {
