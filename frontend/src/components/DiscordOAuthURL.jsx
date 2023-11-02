087c279ac8fd9025692ebb1dae28148b22451403
Chipy <iamchipy@gmail.com>
2023-11-02 15:04:58 -0400

    Delete frontend/src/components/DiscordOAuthURL.jsx

--- frontend/src/components/DiscordOAuthURL.jsx
+++
@@ -1,6 +0,0 @@
-const devDiscordOAuthURI = "https://discord.com/api/oauth2/authorize?client_id=1168939215367721021&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth&response_type=token&scope=identify%20guilds"
-const prodDiscordOAuthURI = "https://discord.com/api/oauth2/authorize?client_id=1168939215367721021&redirect_uri=https%3A%2F%2Fbreeding.chipy.dev%2Foauth&response_type=token&scope=identify%20guilds"
-
-const discordOAuthURL = prodDiscordOAuthURI
-
-export default discordOAuthURL
