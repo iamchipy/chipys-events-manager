c8324fe84d5baecacfcfcd58de1b21abe4cefe67
chipy <iamchipy@gmail.com>
2023-11-03 09:57:15 -0400

    Added footer with GitHub link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Container className="my-2">
         <Outlet />
       </Container>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
+import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { LinkContainer } from 'react-router-bootstrap'
+import { useLogoutMutation } from '../slices/userApiSlice';
+import { logout } from '../slices/authSlice';
+import { useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <p className='text-center p-2 '>
+                    Built & maintained by <b>chipy</b>. More info on this project on <a href="https://github.com/iamchipy/chipys-breeding-manager">GitHub</a>
+                </p>
+
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
